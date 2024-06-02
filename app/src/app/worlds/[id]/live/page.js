9e2e39e2b4a9044b38661166e086829f39175c40
Calderis <Calderis@users.noreply.github.com>
2024-06-02 13:02:46 +0200

    Add Scene composant

--- app/src/app/worlds/[id]/live/page.js
+++ app/src/app/worlds/[id]/live/page.js
@@ -13,6 +13,7 @@ import AudioRecorder from "@/utils/audio-recorder";
 import { transcribeAudio } from "@/utils/transcript";
 import { generateContext, generateImage } from "@/utils/mistral";
 import { updateWorld } from '@/utils/directus';
+import Scene from '@/components/scene';
 
 const batchSize = 1;
 
@@ -139,44 +140,6 @@ export default function EditWorld() {
         {!recording && <span className="text-gray-500">Démarrer l'enregistrement</span>}
       </div>
 
-      <aside className="sidebar">
-        <div className="sidebar-body">
-          {(currentWorld?.scenes || []).map((scene) => (
-            <div
-              key={scene.index}
-              className={`sidebar-item ${currentScene && scene.index === currentScene.index ? 'current' : ''}`}
-              onClick={() => handleSceneClick(scene)}
-            >
-              {/* {scene.loading ? (
-                <svg
-                  className="animate-spin -ml-1 mr-1 h-5 w-5 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-              ) : (
-                <i className="fas fa-image" />
-              )}
-              <span>{scene.name}</span> */}
-            </div>
-          ))}
-        </div>
-      </aside>
-
       {currentScene && (
         <section className="flex">
           <div className="flex flex-col gap-5 items-center">
@@ -211,6 +174,7 @@ export default function EditWorld() {
           </div>
         </section>
       )}
+      {currentScene && <Scene scene={currentScene} />}
     </div>
   );
 }
