7d06be39ce7f9ecf25f76cd511af73249a943487
Antoine Mairesse <antoine.mairesse@free.fr>
2024-07-09 11:01:13 +0200

    Fix routing

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -2,7 +2,7 @@ import "../../css/index.css";
 import { ClientOnly } from "./client";
 
 export function generateStaticParams() {
-    return [{ slug: [""] }];
+    return [{ slug: [] }];
 }
 
 export default function Page() {
